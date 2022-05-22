a5bc1460b1b9dd7a0c5ab8f2e648bc7c35a1ba69
sudha-github22 <sudha.jan90@gmail.com>
2022-05-22 13:32:47 +0100

    initial commit

---
+++ src/main/java/com/spring5/ssddi/SsdDiApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.ssddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring5/ssddi/SsdDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.ssddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
